5f9edeb6215d7c9b559d88926367dc457410286d
LincKim <lincolnkimutai@gmail.com>
2023-03-27 20:57:29 +0300

    Added login functionality

--- src/components/login.js
+++ src/components/login.js
@@ -3,75 +3,51 @@ import React, { useState } from "react";
 
 function Login(props) {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [isLoggedin, setIsLoggedin] = useState(false);
-   
-    const login = (e) => {
-      e.preventDefault();
-      console.log(name, email, password);
-      const userData = {
-        name,
-        email,
-        password,
-      };
-      localStorage.setItem('token-info', JSON.stringify(userData));
-      setIsLoggedin(true);
-      setName('');
-      setEmail('');
-      setPassword('');
-    };
-   
-    const logout = () => {
-      localStorage.removeItem('token-info');
-      setIsLoggedin(false);
-    };
-   
-    return (
-      <>
-        <div  className="login">
-           
-          {!isLoggedin ? (
-            
-              <form action="" onClick={login} className="login">
-                <input
-                  type="text"
-                  onChange={(e) => setName(e.target.value)}
-                  value={name}
-                  placeholder="Name"
-                  id="inputs"
-                />
-                <input
-                  type="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  value={email}
-                  placeholder="Email"
-                  id="inputs"
-                />
-                <input
-                  type="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  placeholder="Password"
-                  id="inputs"
-                />
-                <button  id="buttons"type="submit" >
-                  login
-                </button>
-              </form>
-             
-          ) : (
-            
-              <button onClickCapture={logout}>logout</button>
-              
-          )}
-        </div>
-        <button onClick={()=>props.onFormSwitch("Register")}>Dont have an account? Register</button>
-      </>
-    );
-  
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const handleLogin = async (event) => {
+      event.preventDefault();
+      const response = await fetch('', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+          
+      });
+     
+      const data = await response.json();
+      console.log(data);
+  };
+
+  return (
+      <div className="login">
+       <form onSubmit={handleLogin} className="" >
+       <h1 className="">Sign In</h1>
+          <div className="">
+          <label>Email</label>
+          <input className="" 
+             type="text" 
+             placeholder="Enter email" 
+             value={email} onChange={(e) => setEmail(e.target.value)} 
+             required
+          /> 
+          </div>
+          <div className="w-1/2">
+          <label>Password</label>
+          <input className="" 
+             type="password" 
+             placeholder="Enter password" 
+             value={password} onChange={(e) => setPassword(e.target.value)} 
+             required
+          /> 
+          </div>
+          <button>Log in</button>
+       </form>
+           
+      </div>
+  );
 
 }
 
